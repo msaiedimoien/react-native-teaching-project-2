a8c069bf6a468008e56ab35f113ec81ba6ea9414
Mostafa Saeidi <msaiedimoien@yahoo.com>
2022-09-12 23:59:47 +0430

    - Add some Styling

--- src/screens/IndexScreen.jsx
+++ src/screens/IndexScreen.jsx
@@ -1,13 +1,13 @@
 import React, {useContext} from "react";
 import {View, Text, StyleSheet, FlatList, Button} from "react-native";
 import { Context } from "../context/BlogContext";
+import { Feather } from '@expo/vector-icons';
 
 const IndexScreen = () => {
     const {state, addBlogPost} = useContext(Context);
 
     return (
         <View>
-            <Text>Index Screen!</Text>
             <Button
                 title='Add Post'
                 onPress={addBlogPost}
@@ -15,12 +15,35 @@ const IndexScreen = () => {
             <FlatList
                 data={state}
                 keyExtractor={(e) => e.title}
-                renderItem={({item}) => <Text>{item.title}</Text>}
+                renderItem={({item}) => {
+                    return <View style={styles.row}>
+                        <Text style={styles.title}>{item.title}</Text>
+                        <Feather style={styles.icon} name="trash"/>
+                    </View>
+                }}
             />
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: "row",
+        justifyContent: 'space-between',
+        paddingVertical: 20,
+        paddingHorizontal: 10,
+        borderColor: 'gray',
+        borderTopWidth: 1,
+    },
+
+    title: {
+        fontSize: 18,
+    },
+
+    icon: {
+        fontSize: 24,
+        color: 'tomato'
+    },
+});
 
 export default IndexScreen;
